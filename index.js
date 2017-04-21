4d802762c5601984ff085cb958fec95213c97fcc
Chris Carey <chris.carey@availity.com>
2017-04-21 13:33:29 -0400

    01 : creating routes

--- index.js
+++ index.js
@@ -13,7 +13,20 @@ class api {
     }
 
     post (input) {
-      return this.put(input);
+      var config = {
+        headers: {'Content-Type': 'multipart/form-data'}
+      }
+      return axios.post('http://localhost:7700/pdm/v1/validation2',
+          input,
+          config
+        )
+        .then(function(response) {
+          console.log(response.data);
+          console.log(response.status);
+          console.log(response.statusText);
+          console.log(response.headers);
+          console.log(response.config);
+        });
     }
 
     put (input) {
