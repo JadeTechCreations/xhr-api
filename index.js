15bb55d276df9b144647c1f55e34056aaa7a4329
Chris Carey <chris.carey@availity.com>
2017-04-21 00:40:37 -0400

    01 : creating routes

--- index.js
+++ index.js
@@ -1,6 +1,23 @@
+var request = require('request');
+
 class api {
-    static get (input) {
-      console.log(input);
+    get (input) {
+      return request('http://www.google.com', function (error, response, body) {
+        console.log('error:', error); // Print the error if one occurred
+        console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
+        console.log('body:', body); // Print the HTML for the Google homepage.
+      });
+    }
+
+    post (input) {
+      return input.toString(2);
+    }
+
+    put (input) {
+      return input.toString(2);
+    }
+
+    delete (input) {
       return input.toString(2);
     }
 }
