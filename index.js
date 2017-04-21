bfc9e93736878864da51ab454b1ace4f9cfff4ab
Chris Carey <chris.carey@availity.com>
2017-04-20 22:30:55 -0400

    01 : adding base

---
+++ index.js
@@ -0,0 +1,22 @@
+var express    = require('express');        // call express
+var app        = express();
+
+var bodyParser = require('body-parser');
+
+// configure app to use bodyParser()
+// this will let us get the data from a POST
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+// ROUTES FOR OUR API
+// =============================================================================
+var router = express.Router();              // get an instance of the express Router
+
+// test route to make sure everything is working (accessed at GET http://localhost:8080/api)
+router.get('/', function(req, res) {
+    res.json({ message: 'hooray! welcome to our api!' });
+});
+
+// REGISTER OUR ROUTES -------------------------------
+// all of our routes will be prefixed with /api
+app.use('/api', router);
