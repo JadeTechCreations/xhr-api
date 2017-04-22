129015f606fc2e5c37df1c217735f2bc4d541a29
Chris Carey <chris.carey@availity.com>
2017-04-21 23:19:19 -0400

    01 : creating routes

--- index.js
+++ index.js
@@ -1,54 +1,52 @@
 var axios = require('axios');
+var BASE_URL = 'http://localhost:7700';
 
 class api {
-    get (input) {
-      return axios.get('https://api.github.com/users/codeheaven-io')
-        .then(function(response) {
-          console.log(response.data);
-          console.log(response.status);
-          console.log(response.statusText);
-          console.log(response.headers);
-          console.log(response.config);
-        });
-    }
 
-    post (input) {
-      var config = {
-        headers: {'Content-Type': 'multipart/form-data'}
-      }
-      return axios.post('http://localhost:7700/pdm/v1/validation2',
-          input,
-          config
-        )
-        .then(function(response) {
-          console.log(response.data);
-          console.log(response.status);
-          console.log(response.statusText);
-          console.log(response.headers);
-          console.log(response.config);
-        });
-    }
+  get (url) {
+    return axios.get(BASE_URL+url)
+      .then(function(response) {
+        return response.data;
+      });
+  }
 
-    put (input) {
-      var config = {
-        headers: {'Content-Type': 'multipart/form-data'}
-      }
-      return axios.put('http://localhost:7700/pdm/v1/validation2',
-          input,
-          config
-        )
-        .then(function(response) {
-          console.log(response.data);
-          console.log(response.status);
-          console.log(response.statusText);
-          console.log(response.headers);
-          console.log(response.config);
-        });
+  post (input) {
+    var config = {
+      headers: {'Content-Type': 'multipart/form-data'}
     }
+    return axios.post('http://localhost:7700/pdm/v1/validation2',
+        input,
+        config
+      )
+      .then(function(response) {
+        console.log(response.data);
+        console.log(response.status);
+        console.log(response.statusText);
+        console.log(response.headers);
+        console.log(response.config);
+      });
+  }
 
-    delete (input) {
-      return input.toString(2);
+  put (input) {
+    var config = {
+      headers: {'Content-Type': 'multipart/form-data'}
     }
+    return axios.put('http://localhost:7700/pdm/v1/validation2',
+        input,
+        config
+      )
+      .then(function(response) {
+        console.log(response.data);
+        console.log(response.status);
+        console.log(response.statusText);
+        console.log(response.headers);
+        console.log(response.config);
+      });
+  }
+
+  delete (input) {
+    return input.toString(2);
+  }
 }
 
 export default api;
